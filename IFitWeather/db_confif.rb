39249f0b9bc9ae636a356c7fc36f2752e5080622
Martina Ricci <martinaricci@Martinas-MacBook-Air.local>
2018-04-18 12:43:33 +1000

    second commit

---
+++ IFitWeather/db_confif.rb
@@ -0,0 +1,7 @@
+options = {
+  adapter: 'postgresql',
+  database: 'ifitweather'
+}
+
+#it establishes a connection with heroku dababase, if it fails, it goes back to your own.
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
--- IFitWeather/main.rb
+++ IFitWeather/main.rb
@@ -1,11 +1,14 @@
-     
+require 'pry'
+require_relative 'db_config'    
 require 'sinatra'
 
 get '/' do
   erb :index
 end
 
-
+get '/signup' do
+  erb :signup
+end
 
 
 
