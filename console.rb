2da5f08289667d1697381e434d73d1c3bf9c7d89
Martina Ricci <martinaricci93@live.it>
2018-04-23 10:04:43 +1000

    ready for deployment

--- console.rb
+++ console.rb
@@ -6,4 +6,4 @@ require_relative 'db_config'
 require_relative 'models/user'
 require_relative 'models/activities'
 
-binding.pry
+
--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'sinatra/reloader'
 require 'active_record'
 require 'pg'
 require 'pry'
